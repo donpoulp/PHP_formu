fe6b710ecafb19ac66cabd820f11ebd367b8efb1
laurent Beauvisage <laurentbeauvisage@gmail.com>
2017-01-09 16:11:32 +0100

    Fix Form name.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <div class="col-md-6">
         <div class="jumbotron">
             <p class="text-center">
-                <h2>Formulaire Utilisateur</h2>
+                <h2>Formulaire #1: Utilisateur</h2>
                 <p><a class="btn btn-primary btn-lg" href="/user/create" role="button">Go !</a></p>
             </p>
         </div>
@@ -13,7 +13,7 @@
     <div class="col-md-6">
         <div class="jumbotron">
             <p class="text-center">
-                <h2>Formulaire News</h2>
+                <h2>Formulaire #2: News</h2>
                 <p><a class="btn btn-primary btn-lg" href="/news/create" role="button">Go !</a></p>
             </p>
         </div>
